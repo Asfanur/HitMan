9e10a572b8052183607ef0ce1dadffa97b9149e3
Asfanur <asfanur.arafin@gmail.com>
2015-07-20 21:25:35 +1000

    added network model downloader

---
+++ HitMan/NetworkModelDownloader.h
@@ -0,0 +1,13 @@
+//
+//  NetworkModelDownloader.h
+//  HitMan
+//
+//  Created by Asfanur Arafin on 20/07/2015.
+//  Copyright (c) 2015 Asfanur Arafin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NetworkModelDownloader : NSObject
+
+@end
---
+++ HitMan/NetworkModelDownloader.m
@@ -0,0 +1,13 @@
+//
+//  NetworkModelDownloader.m
+//  HitMan
+//
+//  Created by Asfanur Arafin on 20/07/2015.
+//  Copyright (c) 2015 Asfanur Arafin. All rights reserved.
+//
+
+#import "NetworkModelDownloader.h"
+
+@implementation NetworkModelDownloader
+
+@end
